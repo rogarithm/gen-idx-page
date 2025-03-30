d29f52fab4cfc8c8745d9d237a33ce0a903e3ad8
sehoon.gim <sehoongim@gmail.com>
2025-03-30 16:30:50 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/org/gsh/genidxpage/GenidxpageApplication.java
@@ -0,0 +1,13 @@
+package org.gsh.genidxpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenidxpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenidxpageApplication.class, args);
+	}
+
+}
