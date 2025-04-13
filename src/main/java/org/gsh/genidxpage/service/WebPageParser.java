aecd948ed8472086c1060a3e093c157ae58ae730
sehoon.gim <sehoongim@gmail.com>
2025-04-13 20:22:50 +0900

    feat: 태그 규칙에 맞는 html 요소를 검색할 수 있다

--- src/main/java/org/gsh/genidxpage/service/WebPageParser.java
+++ src/main/java/org/gsh/genidxpage/service/WebPageParser.java
@@ -1,2 +1,11 @@
-package org.gsh.genidxpage.service;public class WebPageParser {
+package org.gsh.genidxpage.service;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class WebPageParser {
+
+	public Elements findBy(Document doc, String cssQuery) {
+		return doc.select(cssQuery);
+	}
 }
