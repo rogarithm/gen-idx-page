6a2fdda09d9a699c8af00ba1be7aad4776b77334
sehoon.gim <sehoongim@gmail.com>
2025-04-15 18:04:39 +0900

    refactor: findPostLinks 입력 타입을 고친다
     - 실제로 쓸 때 컨트롤러는 서버에서 문자열을 받아서 WebPageParser에
       넘긴다
     - 컨트롤러에서 Document를 어떻게 만드는지 알 필요는 없다. 이 연산은
       WebPageParser에서 담당하도록 한다

--- src/main/java/org/gsh/genidxpage/service/WebPageParser.java
+++ src/main/java/org/gsh/genidxpage/service/WebPageParser.java
@@ -1,11 +1,13 @@
 package org.gsh.genidxpage.service;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class WebPageParser {
 
-	public Elements findPostLinks(Document doc) {
+	public Elements findPostLinks(String stringDoc) {
+		Document doc = Jsoup.parse(stringDoc);
 		return doc.select(".POST_BODY > a");
 	}
 }
