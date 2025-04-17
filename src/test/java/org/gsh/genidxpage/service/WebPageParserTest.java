30eb79ba507a91a8dbd48d5233fbfd00b4c10c27
sehoon.gim <sehoongim@gmail.com>
2025-04-18 01:15:13 +0900

    test: 클라이언트에 돌려줄 형태로 html 요소 검색 결과를 변환할 수 있다
     - 연산을 PostLinkInfo에서 WebPageParser로 옮긴다

--- src/test/java/org/gsh/genidxpage/service/WebPageParserTest.java
+++ src/test/java/org/gsh/genidxpage/service/WebPageParserTest.java
@@ -42,4 +42,26 @@ public class WebPageParserTest {
             Assertions.assertThat(postLink.getPageUrl()).isNotNull();
         }
     }
+
+    @DisplayName("클라이언트에 돌려줄 형태로 html 요소 검색 결과를 변환할 수 있다")
+    @Test
+    public void convert_matching_html_elems_for_client() throws IOException {
+        WebPageParser webPageParser = new WebPageParser();
+
+        Path path = Paths.get("src/test/resources/2021-03-full-response.html");
+        String fileContent = Files.readString(path, StandardCharsets.UTF_8);
+        List<PostLinkInfo> postLinks = webPageParser.findPostLinks(fileContent);
+        Assertions.assertThat(webPageParser.buildPageLinks(postLinks)).isEqualTo(
+            "<a href=\"https://web.archive.org/web/20230614220926/http://agile.egloos.com/5946833\">올해 첫 AC2 과정 40기가 곧 열립니다</a>"
+        );
+
+        Path path2 = Paths.get("src/test/resources/2020-02-response.html");
+        String fileContent2 = Files.readString(path2, StandardCharsets.UTF_8);
+        List<PostLinkInfo> postLinks2 = webPageParser.findPostLinks(fileContent2);
+        Assertions.assertThat(webPageParser.buildPageLinks(postLinks2)).isEqualTo(
+            "<a href=\"https://web.archive.org/web/20230614124528/http://agile.egloos.com/5932600\">AC2 온라인 과정 : 마인크래프트로 함께 자라기를 배운다</a>"
+                + "\n" +
+                "<a href=\"https://web.archive.org/web/20230614124528/http://agile.egloos.com/5931859\">혹독한 조언이 나를 살릴까?</a>"
+        );
+    }
 }
