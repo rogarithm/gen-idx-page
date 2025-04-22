36d7297fa4b6f3bbad018b3e0839da4b1a31f216
sehoon.gim <sehoongim@gmail.com>
2025-04-23 01:42:02 +0900

    fix: ApiCallReporter는 padding 없는 month 값을 쓴다
     - ApiCallReporter의 메서드 호출 시 쓰는 month 값은
       CheckPostArchivedDto의 month 필드이기 때문에 값이 한자리인 경우에도
       앞에 0이 붙지 않는다. 테스트에서 이 규칙을 따르지 않는 값을 고친다

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO page_url_report (year, month, page_exists, created_at)
-VALUES ('2021', '03', true, '2025-04-21T20:32:02.673678');
+VALUES ('2021', '1', true, '2025-04-21T20:32:02.673678');
