d86d6cb9da44645579b29f05d86de6314f57d51f
sehoon.gim <sehoongim@gmail.com>
2025-04-27 21:38:20 +0900

    fix: github action ci 스크립트 로직 고친다
     - 기존 check_code_format.rb는 checkstyle 설정에 맞지 않으면
       editorconfig를 확인하지 않은 채로 끝난다
     - checkstyle 설정에 맞지 않을 경우에도 editorconfig 설정까지 확인하도록
       한다

--- hooks/check_code_format.rb
+++ hooks/check_code_format.rb
@@ -5,20 +5,23 @@ is_checkstyle_failed = checkstyle_result.split("\n")
                                         .select {|l|
                                           l.match(/^> Task :checkstyleMain FAILED.*|^> Task :checkstyleTest FAILED.*/)
                                         } != []
-if is_checkstyle_failed
-  puts 'failed to format code as configured in checkstyle!'
-  puts 'run ./gradlew check for detail'
-  exit 1
-end
 
 editorconfig_result = `./gradlew editorconfigCheck 2>/dev/null`
 is_editorconfig_failed = editorconfig_result.split("\n")
                                             .select {|l|
                                               l.match(/^> Task :editorconfigCheck FAILED.*/)
                                             } != []
+
+if is_checkstyle_failed
+  puts 'failed to format code as configured in checkstyle!'
+  puts 'run ./gradlew check for detail'
+end
 if is_editorconfig_failed
   puts 'trying to recover editorconfig failure...'
   puts 'commit before push'
   puts 'run ./gradlew editorConfigFormat'
+end
+
+if is_checkstyle_failed or is_editorconfig_failed
   exit 1
 end
