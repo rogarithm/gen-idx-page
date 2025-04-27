2b158043853651dd8f5a4a5c4895b4275465c39e
sehoon.gim <sehoongim@gmail.com>
2025-04-27 21:22:17 +0900

    fix: 오류 고친다

--- hooks/check_branch_name.rb
+++ hooks/check_branch_name.rb
@@ -3,6 +3,8 @@
 now_branch_nm = `git rev-parse --abbrev-ref HEAD`
 branch_nm_convention = /^issue\/[1-9][0-9]*(\w|-)+/
 
+puts "current branch name is #{now_branch_nm}"
+
 if now_branch_nm.match(branch_nm_convention).nil?
   puts 'branch name does not follow the convention!'
   puts 'it should match "issue/80-blah-blah"'
