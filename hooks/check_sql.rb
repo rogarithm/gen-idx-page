10a7288bba4549263aabe3f35d68bb8d41325de0
sehoon gim <sehoongim@gmail.com>
2025-04-30 00:57:09 +0900

    chore: 커밋 전에 mybatis xml 내 sql 문법 체크한다 (#33)
    - 현재 git index에 포함된 변경사항 중 mybatis xml이 있을 경우, sql 문법 체크한다. 에러가 날 경우, 커밋을 중단한다
    - sql 문법 체크에는 sqlfluff를 쓴다. `--rules PRS`로 포맷 관련 에러는 무시한다

---
+++ hooks/check_sql.rb
@@ -0,0 +1,57 @@
+require 'nokogiri'
+
+# 컬럼 타입에 따른 리터럴 값을 설정한다
+def dummy_value_for(mysql_type)
+  case mysql_type
+  when /int/, /bigint/ then "1"
+  when /bool/ then "true"
+  when /char/, /text/, /varchar/ then "'sample'"
+  when /date/, /time/ then "'2024-01-01 00:00:00'"
+  when /decimal/, /float/, /double/ then "1.23"
+  else "null"
+  end
+end
+
+# DB 테이블 스키마 파일로부터 컬럼별 필드를 알아낸다
+def fetch_column_types(schema_path, table_name)
+  schema = File.read(schema_path)
+
+  table_def = schema[/CREATE TABLE `#{table_name}`.*?\);/m]
+  return {} unless table_def
+
+  types = {}
+  table_def.scan(/\s*`?(\w+)`?\s+(\w+),?/) do |col, type|
+    types[col] = type
+  end
+  types
+end
+
+# XML에서 SQL 추출 후 리터럴을 대입한다
+def extract_and_substitute(xml_path, table_name)
+  doc = Nokogiri::XML(File.read(xml_path))
+  types = fetch_column_types("./src/main/resources/sql/schema.sql", table_name)
+
+  sqls = doc.xpath("//insert | //update | //select | //delete").map do |node|
+    raw_sql = node.text.strip
+
+    substituted_sql = raw_sql.gsub(/^(.*)(#\{(\w+)})(.*)$/) do
+      left = $1
+      camel_param = $3
+      right = $4
+      snake_param = camel_param.gsub(/(.)([A-Z])/,'\1_\2').downcase!
+
+      mysql_type = snake_param == "" ? types[camel_param] : types[snake_param]
+      sub = dummy_value_for(mysql_type || "varchar") # fallback
+      "#{left}#{sub}#{right}"
+    end
+    substituted_sql
+  end
+
+  sqls
+end
+
+sqls = extract_and_substitute("./src/main/resources/mapper/WebArchiveReportMapper.xml", "page_url_report")
+sqls.each_with_index do |sql, idx|
+  File.write("./hooks/.sql_check/sql_#{idx + 1}.sql", sql)
+  puts `sqlfluff lint ./hooks/.sql_check/sql_#{idx + 1}.sql --dialect mysql --rules PRS`
+end
