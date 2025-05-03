080c7b688fd7249f606e72dd4ef17062c59a042a
sehoon.gim <sehoongim@gmail.com>
2025-05-03 15:20:06 +0900

    feat: 설정한 일정에 맞춰 여러 요청을 보낸다

--- src/main/java/org/gsh/genidxpage/scheduler/WebArchiveScheduler.java
+++ src/main/java/org/gsh/genidxpage/scheduler/WebArchiveScheduler.java
@@ -1,8 +1,31 @@
 package org.gsh.genidxpage.scheduler;
 
+import org.gsh.genidxpage.service.ArchivePageService;
+import org.gsh.genidxpage.service.BulkRequestSender;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class WebArchiveScheduler {
 
-    public void doSend() {
+    private final BulkRequestSender bulkRequestSender;
+    private final ArchivePageService archivePageService;
 
+    public WebArchiveScheduler(
+        BulkRequestSender bulkRequestSender,
+        ArchivePageService archivePageService
+    ) {
+        this.bulkRequestSender = bulkRequestSender;
+        this.archivePageService = archivePageService;
+    }
+
+    public void scheduleSend() {
+        doSend();
+    }
+
+    public void doSend() {
+        List<String> yearMonths = bulkRequestSender.prepareInput();
+        bulkRequestSender.sendAll(yearMonths, archivePageService);
     }
 }
