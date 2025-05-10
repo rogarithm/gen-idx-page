5e7522ed15b0893f6754e1bc2efaa0bca0cec83d
sehoon.gim <sehoongim@gmail.com>
2025-05-10 16:57:43 +0900

    chore: 깃헙액션 브랜치명 규칙 수정한다

--- hooks/check_branch_name.rb
+++ hooks/check_branch_name.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
 now_branch_nm = ARGV[0]
-branch_nm_convention = /^issue\/[1-9][0-9]*(\w|-)+/
+branch_nm_convention = /^issue-[1-9][0-9]*\/\w{1}(\w|-)*/
 
 puts "current branch name is #{now_branch_nm}"
 
 if now_branch_nm.match(branch_nm_convention).nil?
   puts 'branch name does not follow the convention!'
-  puts 'it should match "issue/80-blah-blah"'
+  puts 'it should match "issue-80/blah-blah"'
   exit 1
 end
