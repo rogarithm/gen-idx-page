aa05b995cb40ab001316ef6c47b176540e149bfb
sehoon.gim <sehoongim@gmail.com>
2025-05-13 00:00:15 +0900

    feat: 블로그 목록 페이지의 url을 db에 기록한다

--- src/main/java/org/gsh/genidxpage/service/PostListPageRecorder.java
+++ src/main/java/org/gsh/genidxpage/service/PostListPageRecorder.java
@@ -1,10 +1,19 @@
 package org.gsh.genidxpage.service;
 
+import org.gsh.genidxpage.dao.PostListPageMapper;
 import org.gsh.genidxpage.entity.PostListPage;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class PostListPageRecorder {
 
-    void record(PostListPage postListPage) {}
+    private final PostListPageMapper mapper;
+
+    public PostListPageRecorder(PostListPageMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    void record(PostListPage postListPage) {
+        mapper.insertPostListPage(postListPage);
+    }
 }
