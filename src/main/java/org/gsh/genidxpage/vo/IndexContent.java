b51753b02c3bdec8a55bffa2526dddd91523213a
sehoon.gim <sehoongim@gmail.com>
2025-05-22 19:59:37 +0900

    feat: IndexContent 필드 일반화한다
     - year, month는 블로그 목록을 구분하는 데 쓴다. 하지만 다른 블로그의
       경우 연월이 아닌 다른 기준으로 블로그 목록을 구분할 수 있다
     - 이러한 요구사항을 대비해서 더 일반화된 필드인 postListId를 도입한다

--- src/main/java/org/gsh/genidxpage/vo/IndexContent.java
+++ src/main/java/org/gsh/genidxpage/vo/IndexContent.java
@@ -2,24 +2,23 @@ package org.gsh.genidxpage.vo;
 
 public class IndexContent {
 
-    private String year;
-    private String month;
+    private String postListId;
     private String rawHtml;
 
     private IndexContent() {}
 
-    IndexContent(String year, String month, String rawHtml) {
-        this.year = year;
-        this.month = month;
+    IndexContent(String postListId, String rawHtml) {
+        this.postListId = postListId;
         this.rawHtml = rawHtml;
     }
 
     public static IndexContent from(String year, String month, String rawHtml) {
-        return new IndexContent(year, month, rawHtml);
+        String postListId =  String.format("%s/%s", year, month);
+        return new IndexContent(postListId, rawHtml);
     }
 
     @Override
     public String toString() {
-        return String.format("%s/%s:%s", year, month, rawHtml);
+        return String.format("%s:%s", postListId, rawHtml);
     }
 }
