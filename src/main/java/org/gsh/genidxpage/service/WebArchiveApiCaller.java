44ddd83323ab267156dcc6af80f3f04180ba435f
sehoon.gim <sehoongim@gmail.com>
2025-05-28 22:28:56 +0900

    style: checkstyle에 맞게 포맷 고친다

--- src/main/java/org/gsh/genidxpage/service/WebArchiveApiCaller.java
+++ src/main/java/org/gsh/genidxpage/service/WebArchiveApiCaller.java
@@ -13,7 +13,6 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
