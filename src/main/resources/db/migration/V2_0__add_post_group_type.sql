f481dde1c0f4f3457593068d74f937b6664fafc5
sehoon.gim <sehoongim@gmail.com>
2025-06-16 00:49:03 +0900

    refactor: 블로그 그룹핑 기준을 일반화하기 위해 DB 스키마 수정한다
     - 기존 연월 기반에서 블로그 인덱스를 구분하던 방식에서 확장 가능하도록
       구조를 변경한다
     - 블로그 인덱스 구분 기준을 저장하는 post_group_type 테이블을 추가한다
     - post_list_page_status, post_list_page 테이블에 post_group_type_id,
       group_key 컬럼을 추가한다. post_group_type을 참조하고 기준값을
       문자열로 저장한다
     - 기존에 year, month 컬럼에 저장된 값을 post_group_type을 이용한
       로직에서 쓸 수 있도록 group_key에 복사한다. year, month에 있는 값은
       추후 점진적으로 바꿀 예정이다

---
+++ src/main/resources/db/migration/V2_0__add_post_group_type.sql
@@ -0,0 +1,13 @@
+# 연월 이외 다른 기준으로 블로그 글을 그룹핑하는 블로그도 처리할 수 있도록 블로그 글 그룹핑 기준 테이블을 추가한다
+CREATE TABLE post_group_type
+(
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    value      VARCHAR(255)       NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME
+);
+
+INSERT INTO post_group_type (value, created_at)
+VALUES ('year_month', CURRENT_DATE()),
+       ('category', CURRENT_DATE());
