a5e57dce6927f3090b7b09394ca4d1f76c6d23b6
sehoon.gim <sehoongim@gmail.com>
2025-06-16 22:28:49 +0900

    fix: H2와 호환되지 않는 sql 구문을 고친다
     - 주석에 # 대신 --
     - 예약어인 value 대신 group_type
     - ALTER TABLE문 하나에 여러 연산 동시에 하지 않도록

--- src/main/resources/db/migration/V2_1__apply_group_type_to_post_list_page_status.sql
+++ src/main/resources/db/migration/V2_1__apply_group_type_to_post_list_page_status.sql
@@ -1,16 +1,18 @@
-# 기존 post_list_page_status 테이블의 데이터가 새로 추가한 post_group_type에 연결되도록 업데이트한다
+-- 기존 post_list_page_status 테이블의 데이터가 새로 추가한 post_group_type에 연결되도록 업데이트한다
 ALTER TABLE post_list_page_status
-    ADD COLUMN post_group_type_id BIGINT,
+    ADD COLUMN post_group_type_id BIGINT;
+ALTER TABLE post_list_page_status
     ADD COLUMN group_key VARCHAR(255);
 
 UPDATE post_list_page_status
 SET post_group_type_id = (SELECT id
                           FROM post_group_type pgt
-                          WHERE pgt.value = 'year_month'),
+                          WHERE pgt.group_type = 'year_month'),
     group_key    = CONCAT(year, '/', month);
 
 ALTER TABLE post_list_page_status
-    MODIFY post_group_type_id BIGINT NOT NULL,
+    MODIFY post_group_type_id BIGINT NOT NULL;
+ALTER TABLE post_list_page_status
     MODIFY group_key VARCHAR (255) NOT NULL;
 
 ALTER TABLE post_list_page_status
