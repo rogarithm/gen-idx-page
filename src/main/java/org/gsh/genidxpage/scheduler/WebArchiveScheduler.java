8a9d6657096e742d5e12b17431694d0a27ca8aae
sehoon.gim <sehoongim@gmail.com>
2025-06-20 16:50:59 +0900

    refactor: WebArchiveJob 클래스를 도입한다
     - 새로운 블로그의 인덱스 페이지를 만들기 위해 새로운 BulkRequestSender,
       ArchivePageService, IndexPageGenerator 클래스를 정의해야 한다
     - WebArchiveScheduler에서 모든 인덱스 페이지를 만들 수 있게 하려면,
       기존 클래스와 새로 만든 클래스를 모두 알고 있어야 한다.  이렇게
       바꿨을 경우, doSend() 등 WebArchiveScheduler 내부에서 호출하는 메서드에
       속성이 추가되어야 해서 유지보수가 어려워진다
     - 각 블로그 인덱스 페이지 생성을 담당하는 빈을 갖는 WebArchiveJob을 정의해서
       WebArchiveScheduler는 단순히 WebArchiveJob에 요청만 하도록 한다

--- src/main/java/org/gsh/genidxpage/scheduler/WebArchiveScheduler.java
+++ src/main/java/org/gsh/genidxpage/scheduler/WebArchiveScheduler.java
@@ -1,84 +1,25 @@
 package org.gsh.genidxpage.scheduler;
 
-import org.gsh.genidxpage.service.ArchivePageService;
-import org.gsh.genidxpage.service.IndexPageGenerator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Component
 public class WebArchiveScheduler {
 
-    private final BulkRequestSender bulkRequestSender;
-    private final ArchivePageService archivePageService;
-    private final IndexPageGenerator indexPageGenerator;
+    private final List<WebArchiveJob> webArchiveJobs;
 
-    public WebArchiveScheduler(BulkRequestSender bulkRequestSender,
-        ArchivePageService archivePageService, IndexPageGenerator indexPageGenerator) {
-        this.bulkRequestSender = bulkRequestSender;
-        this.archivePageService = archivePageService;
-        this.indexPageGenerator = indexPageGenerator;
+    public WebArchiveScheduler(List<WebArchiveJob> webArchiveJobs) {
+        this.webArchiveJobs = webArchiveJobs;
     }
 
     @Scheduled(cron = "0 0 * * * *")
     public void scheduleSend() {
-        doSend();
-        doRetry();
-        List<String> pageLinkList = readIndexContent();
-        doGenerate(pageLinkList);
-    }
-
-    public void doSend() {
-        List<String> yearMonths = bulkRequestSender.prepareInput();
-        reportSendInfo(yearMonths);
-        bulkRequestSender.sendAll(yearMonths, archivePageService);
-    }
-
-    public void doRetry() {
-        List<String> yearMonths = archivePageService.findFailedRequests();
-        reportRetryInfo(yearMonths);
-        bulkRequestSender.sendAll(yearMonths, archivePageService);
-        reportRetryResult(yearMonths);
-    }
-
-    void reportSendInfo(final List<String> sendInfo) {
-        log.info(String.format("Sending %s requests", sendInfo.size()));
-    }
-
-    void reportRetryInfo(final List<String> retryInfo) {
-        log.info(String.format("Retrying for %s failed requests: %s", retryInfo.size(), retryInfo));
-    }
-
-    void reportRetryResult(final List<String> sendInfo) {
-        List<String> failedAfterRetry = archivePageService.findFailedRequests();
-        List<String> successAfterRetry = successAfterRetry(sendInfo, failedAfterRetry);
-
-        log.info(String.format("Failed %s retry requests for: %s", failedAfterRetry.size(),
-            failedAfterRetry));
-        log.info(
-            String.format("Successed %s retry requests for: %s", successAfterRetry.size(),
-                successAfterRetry));
-    }
-
-    List<String> successAfterRetry(List<String> sendInfo, List<String> failedAfterRetry) {
-        List<String> successAfterRetry = new ArrayList<>();
-        for (String aSendInfo : sendInfo) {
-            if (!failedAfterRetry.contains(aSendInfo)) {
-                successAfterRetry.add(aSendInfo);
-            }
+        for (WebArchiveJob webArchiveJob : webArchiveJobs) {
+            webArchiveJob.execute();
         }
-        return successAfterRetry;
-    }
-
-    List<String> readIndexContent() {
-        return indexPageGenerator.readIndexContent();
-    }
-
-    void doGenerate(List<String> pageLinkList) {
-        indexPageGenerator.generateIndexPage(pageLinkList);
     }
 }
