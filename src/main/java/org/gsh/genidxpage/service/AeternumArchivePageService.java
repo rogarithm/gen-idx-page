a5bd400bd77f2a6264ae7522e1a2345acdbe067a
sehoon.gim <sehoongim@gmail.com>
2025-07-09 16:27:51 +0900

    feat: 카테고리 기준 ArchivePageService를 추가한다

---
+++ src/main/java/org/gsh/genidxpage/service/AeternumArchivePageService.java
@@ -0,0 +1,97 @@
+package org.gsh.genidxpage.service;
+
+import org.gsh.genidxpage.repository.ArchiveStatusReporter;
+import org.gsh.genidxpage.repository.PostListPageRecorder;
+import org.gsh.genidxpage.repository.PostRecorder;
+import org.gsh.genidxpage.service.dto.ArchivedPageInfo;
+import org.gsh.genidxpage.service.dto.CheckPostArchived;
+import org.gsh.genidxpage.service.dto.EmptyArchivedPageInfo;
+import org.gsh.genidxpage.vo.GroupKey;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class AeternumArchivePageService implements ArchivePageService {
+    private final WebArchiveApiCaller webArchiveApiCaller;
+    private final ArchiveStatusReporter reporter;
+    private final PostListPageRecorder listPageRecorder;
+    private final PostRecorder postRecorder;
+    private final WebPageParser webPageParser;
+
+    public AeternumArchivePageService(WebArchiveApiCaller webArchiveApiCaller,
+        ArchiveStatusReporter reporter, PostListPageRecorder listPageRecorder, PostRecorder postRecorder,
+        WebPageParser webPageParser) {
+        this.webArchiveApiCaller = webArchiveApiCaller;
+        this.reporter = reporter;
+        this.listPageRecorder = listPageRecorder;
+        this.postRecorder = postRecorder;
+        this.webPageParser = webPageParser;
+    }
+
+    @Transactional
+    @Override
+    public String findBlogPageLink(final CheckPostArchived dto) {
+        ArchivedPageInfo archivedPageInfo = this.findArchivedPageInfo(dto);
+        GroupKey groupKey = GroupKey.from(dto.getGroupKey());
+        if (archivedPageInfo.isUnreachable()) {
+            log.debug(
+                String.format("fail to read blog page link for %s due to timeout",
+                    groupKey)
+            );
+            return "";
+        }
+
+        if (archivedPageInfo.isEmpty()) {
+            log.debug(
+                String.format("empty blog page link for %s", groupKey));
+            return "";
+        }
+        Long listPageId = listPageRecorder.record(groupKey, archivedPageInfo);
+        log.debug("id of post list page inserted/updated now is {" + listPageId + "}");
+
+        String blogPost = this.findBlogPostPage(archivedPageInfo);
+        postRecorder.record(this.buildPageLinks(blogPost), listPageId);
+
+        return this.buildPageLinks(blogPost);
+    }
+
+    @Override
+    public List<String> findFailedRequests() {
+        return reporter.readAllFailedRequestInput();
+    }
+
+    ArchivedPageInfo findArchivedPageInfo(final CheckPostArchived dto) {
+        ArchivedPageInfo archivedPageInfo = webArchiveApiCaller.findArchivedPageInfo(dto.getUrl(),
+            dto.getTimestamp());
+
+        GroupKey groupKey = GroupKey.from(dto.getGroupKey());
+        if (archivedPageInfo.isUnreachable()) {
+            reporter.reportArchivedPageSearch(groupKey, Boolean.FALSE);
+            return archivedPageInfo;
+        }
+
+        if (!webArchiveApiCaller.isArchived(archivedPageInfo)) {
+            reporter.reportArchivedPageSearch(groupKey, Boolean.FALSE);
+            return new EmptyArchivedPageInfo();
+        }
+
+        reporter.reportArchivedPageSearch(groupKey, Boolean.TRUE);
+        return archivedPageInfo;
+    }
+
+    String findBlogPostPage(final ArchivedPageInfo archivedPageInfo) {
+        ResponseEntity<String> blogPostResponse = webArchiveApiCaller.findBlogPostPage(
+            archivedPageInfo);
+        return blogPostResponse.getBody();
+    }
+
+    String buildPageLinks(final String blogPost) {
+        //TODO 새로운 블로그에 맞는 로직을 만들어야 한다
+        return "";
+    }
+}
